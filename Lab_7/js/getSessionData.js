f50305ad9d96c65914c2652fd0a9513f4dd49c76
tedtrabucco <58495028+tedtrabucco@users.noreply.github.com>
2020-11-06 11:56:35 -0500

    Corrected

--- Lab_7/js/getSessionData.js
+++ Lab_7/js/getSessionData.js
@@ -1,9 +1,38 @@
 // Get saved data from sessionStorage
-let title = ""; // Your code here
-let lat_max = "";
-let lat_min = "";
-let lon_max = "";
-let lon_min = "";
+let title = sessionStorage.getItem("title"); // Saves to local variable
+let lat_max = sessionStorage.getItem("latMax");
+let lat_min = sessionStorage.getItem("latMin");
+let lon_max = sessionStorage.getItem("lonMax");
+let lon_min = sessionStorage.getItem("lonMin");
 
-// Your code here
 // Set the corresponding <p> elements in the GetSessionData.html page with the values from above
+
+//Grabs the elements from the GetSessionData.html page
+const titl = document.getElementById("title"); 
+const latMax = document.getElementById("lat_max");
+const latMin = document.getElementById("lat_min");
+const lonMax = document.getElementById("lon_max");
+const lonMin = document.getElementById("lon_min");
+
+// Set the corresponding <p> elements in the GetSessionData.html page with the values from above
+titl.innerText = title;
+latMax.innerText = lat_max;
+latMin.innerText = lat_min;
+lonMax.innerText = lon_max;
+lonMin.innerText = lon_min;
+
+/*MORE EFFICIENT WAY TO DO THIS, ONLY DID ABOVE TO FOLLOW INSTRUCTIONS:
+//NEEDED TO LINK TO GETSESSIONDATA.HTML PAGE
+const title = document.getElementById("title"); 
+const lat_max = document.getElementById("lat_max");
+const lat_min = document.getElementById("lat_min");
+const lon_max = document.getElementById("lon_max");
+const lon_min = document.getElementById("lon_min");
+
+//THIS PULLS DIRECTLY FROM SESSION SO I DO NOT HAVE TO CREATE LOCAL VARIABLE
+title.innerText = sessionStorage.getItem("title");
+lat_max.innerText = sessionStorage.getItem("latMax");
+lat_min.innerText = sessionStorage.getItem("latMin");
+lon_max.innerText = sessionStorage.getItem("lonMax");
+lon_min.innerText = sessionStorage.getItem("lonMin");
+*/
--- Lab_7/js/setSessionData.js
+++ Lab_7/js/setSessionData.js
@@ -1,19 +1,42 @@
 const main = document.getElementById("main");
 
 function getBusRoute() {
-  let busRoute = ""; // Your code here
+  let busRoute = document.getElementById("busroute").value; // grabs the busroute number after button is clicked
 
-  if ((typeof busRoute !== "undefined") & (busRoute !== "")) {
-    let busRouteURL = ""; // Your code here
+  if ((typeof busRoute !== "undefined") & (busRoute !== "")) { //ensures not blank/undefined
+    let busRouteURL = "https://api.umd.io/v0/bus/routes/" + busRoute; // defines where to get route info from
+    
+    fetch(busRouteURL) //grabs info from api
+      .then((response) => { //response is placeholder, can be anything
+        return response.json(); 
+      })
+      .then((route) => {  //route is placeholder
+        let title, latMax, latMin, lonMax, lonMin; //defines elements 
+        
+        if (typeof route.title !== "undefined") {
+          sessionStorage.setItem("title", route.title);   //the following syntax sets website wide values for these, remember route is
+          sessionStorage.setItem("latMax", route.lat_max); //placeholder from line 13
+          sessionStorage.setItem("latMin", route.lat_min);
+          sessionStorage.setItem("lonMax", route.lon_max);
+          sessionStorage.setItem("lonMin", route.lon_min);
 
-    fetch(busRouteURL)
-      .then((response) => {
-        return response.json();
-      })
-      .then((route) => {
-        // YOUR CODE HERE
-      })
-      .catch((err) => {
+          message =
+            sessionStorage.getItem("title") +
+            " <br>Lat max : " +
+            sessionStorage.getItem("latMax") +
+            " <br>Lat min : " +
+            sessionStorage.getItem("latMin") +
+            " <br>Lon Max : " +
+            sessionStorage.getItem("lonMax") +
+            " <br>Lon Min :" +
+            sessionStorage.getItem("lonMin");
+            
+      }
+      else {
+        message = "No bus info available"
+      }
+      main.innerHTML = "Bus Route Info: " + message;})  //returns the HTML for the message (line 23), allowing it to display
+      .catch((err) => {   //error checking
         console.log(err);
         main.innerHTML = "Invalid bus route";
       });
